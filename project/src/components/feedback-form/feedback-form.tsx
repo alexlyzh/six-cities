84eb52c7d53311c5e37a8e189f613628240c3e4a
Alex <al.lyzhin@gmail.com>
2021-10-31 17:09:05 +0300

    Исправление очистки рейтинга

--- project/src/components/feedback-form/feedback-form.tsx
+++ project/src/components/feedback-form/feedback-form.tsx
@@ -49,7 +49,7 @@ function FeedbackForm({id, isSubmitting, onFormSubmit}: ConnectedComponentProps)
         <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"
           onChange={handleFormChange}
           disabled={isSubmitting}
-          checked={review.rating === 5}
+          checked={review.rating === '5'}
         />
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width="37" height="33">
@@ -60,7 +60,7 @@ function FeedbackForm({id, isSubmitting, onFormSubmit}: ConnectedComponentProps)
         <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"
           onChange={handleFormChange}
           disabled={isSubmitting}
-          checked={review.rating === 4}
+          checked={review.rating === '4'}
         />
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
@@ -71,7 +71,7 @@ function FeedbackForm({id, isSubmitting, onFormSubmit}: ConnectedComponentProps)
         <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"
           onChange={handleFormChange}
           disabled={isSubmitting}
-          checked={review.rating === 3}
+          checked={review.rating === '3'}
         />
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
@@ -82,7 +82,7 @@ function FeedbackForm({id, isSubmitting, onFormSubmit}: ConnectedComponentProps)
         <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"
           onChange={handleFormChange}
           disabled={isSubmitting}
-          checked={review.rating === 2}
+          checked={review.rating === '2'}
         />
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
@@ -93,7 +93,7 @@ function FeedbackForm({id, isSubmitting, onFormSubmit}: ConnectedComponentProps)
         <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"
           onChange={handleFormChange}
           disabled={isSubmitting}
-          checked={review.rating === 1}
+          checked={review.rating === '1'}
         />
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
