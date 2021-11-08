fe664b6fa3bd96d74b66ac0379db2f707db0f572
Anton <antoshkajs@yandex-team.ru>
2021-11-08 21:38:46 +0300

    Fix useMap

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -1,7 +1,6 @@
 import {useRef, useEffect} from 'react';
 import 'leaflet/dist/leaflet.css';
-import {Icon, Marker} from 'leaflet';
-import useMap from '../../hooks/useMap';
+import {Icon, Map as LeafletMap, TileLayer, Marker, LeafletEvent} from 'leaflet';
 import {City, Offer} from '../../types/types';
 
 import {AppRoute, MapMarkerURL} from '../../constants';
@@ -26,16 +25,52 @@ const currentCustomIcon = new Icon({
   iconAnchor: [20, 40],
 });
 
+const disableZoom = (evt: LeafletEvent) => evt.target.scrollWheelZoom.disable();
+const enableZoom = (evt: LeafletEvent) => evt.target.scrollWheelZoom.enable();
+
+const LayerSettings = {
+  URL_TEMPLATE: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+  ATTRIBUTION: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+};
+
 function Map(props: MapProps): JSX.Element {
   const {city, offers, highlightedOffer, className} = props;
   const mapRef = useRef(null);
-  const map = useMap(mapRef, city);
+  const map = useRef<LeafletMap | null>(null);
   const history = useHistory();
 
   useEffect(() => {
-    const markers: Marker[] = [];
+    if (mapRef.current !== null && map.current === null) {
+      map.current = new LeafletMap(mapRef.current, {
+        center: {
+          lat: city.location.latitude,
+          lng: city.location.longitude,
+        },
+        zoom: city.location.zoom,
+        scrollWheelZoom: false,
+      });
 
-    if (map) {
+      map.current.addEventListener('click', enableZoom);
+      map.current.addEventListener('mouseout', disableZoom);
+
+      const layer = new TileLayer(LayerSettings.URL_TEMPLATE,{ attribution: LayerSettings.ATTRIBUTION });
+
+      map.current.addLayer(layer);
+    }
+
+    return () => {
+      if (map.current) {
+        map.current.removeEventListener('click', enableZoom);
+        map.current.removeEventListener('mouseout', disableZoom);
+      }
+    };
+  }, [mapRef, map, city]);
+
+  useEffect(() => {
+    const markers: Marker[] = [];
+    const mapCurrent = map.current;
+
+    if (mapCurrent) {
       offers.forEach((offer) => {
         const {id, location} = offer;
         const marker = new Marker({
@@ -52,7 +87,7 @@ function Map(props: MapProps): JSX.Element {
               ? currentCustomIcon
               : defaultCustomIcon,
           )
-          .addTo(map);
+          .addTo(mapCurrent);
       });
     }
     return () => markers.forEach((marker) => marker.remove());
@@ -60,7 +95,7 @@ function Map(props: MapProps): JSX.Element {
 
   useEffect(() => {
     const {location} = city;
-    map?.setView([location.latitude, location.longitude]);
+    map.current?.setView([location.latitude, location.longitude]);
   });
 
   return (
